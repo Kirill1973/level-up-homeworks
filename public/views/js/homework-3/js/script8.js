395d77027919b609f65e8bfe78218c2969217c55
Kirill99 <kirill99.pietkov@gmail.com>
2018-10-13 15:35:15 +0300

    homework-3

---
+++ public/views/js/homework-3/js/script8.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var age = prompt('age', '');
+
+if (age >= 14 && age <= 90) {
+  alert('Hello');
+} else {
+  alert('bad');
+}
---
+++ public/views/js/homework-3/js/script9.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var age = prompt('age', '');
+
+if (!(age >= 14 && age <= 90)) {
+  alert('Hello');
+} else {
+  alert('bad');
+}
+
